192de18e24f59dc69bd2c7d4b46cf77e78399342
swaidenbaum <swaidenbaum@salesforce.com>
2023-05-09 21:38:44 +0300

    last push

--- DatingServer/src/main/java/com/stas/server/controllers/ClientController.java
+++ DatingServer/src/main/java/com/stas/server/controllers/ClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class ClientController {
 
     private ClientLogic clientLogic;
+//    aa
 
     @Autowired
     public ClientController(ClientLogic clientLogic) {
--- DatingServer/src/main/java/com/stas/server/logic/ClientLogic.java
+++ DatingServer/src/main/java/com/stas/server/logic/ClientLogic.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClientLogic {
   private IClientDal iClientDal;
-
+// mm
 
   @Autowired
   public ClientLogic(IClientDal iClientDal) {
